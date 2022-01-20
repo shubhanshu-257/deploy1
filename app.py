88b24f3e2ed7339d2a4db5ad5f2de3640335cf46
Ajay K Gupta <ajay.k.gupta@innovaccer.com>
2022-01-20 16:01:16 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(80), unique=True, nullable=False)
+  content = db.Column(db.String(120), unique=True, nullable=False)
+
+  def __init__(self, title, content):
+    self.title = title
+    self.content = content
+
+
+db.create_all()
+
+
+@app.route('/items/<id>', methods=['GET'])
+def get_item(id):
+  item = Item.query.get(id)
+  del item.__dict__['_sa_instance_state']
+  return jsonify(item.__dict__)
+
+
+@app.route('/items', methods=['GET'])
+def get_items():
+  items = []
+  for item in db.session.query(Item).all():
+    del item.__dict__['_sa_instance_state']
+    items.append(item.__dict__)
+  return jsonify(items)
+
+
+@app.route('/items', methods=['POST'])
+def create_item():
+  body = request.get_json()
+  db.session.add(Item(body['title'], body['content']))
+  db.session.commit()
+  return "item created"
+  
+
+
